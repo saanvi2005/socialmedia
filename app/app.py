bb5bed46aa54cb6bdd5952eaa377f9b02a2b6f13
saanvi2005 <saanvi.kumar17059@gmail.com>
2026-01-10 02:26:36 +0530

    get posts

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-@app.get('/hello_world')
-def hello_world():
-    return {"message": "hello_world"}
+get_posts = {1 : {"title" : "new post", "content": "new cool posts"}}
+
+@app.get("/posts")
+def get_all_posts():
+    return get_posts
+
+@app.get("/posts/{id}")
+def get_post(id : int):
+    if(id not in get_post):
+        return HTTPException(status_code=404, detail="Page not Found")
+    return get_posts.get(id)
